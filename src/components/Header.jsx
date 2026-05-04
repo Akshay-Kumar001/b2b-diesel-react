f33aee3b7ae3951b64e6e0f36f8bfbf6c39a19b5
Akshay Poswal <akshayposwal111@gmail.com>
2026-05-04 23:21:36 +0530

    setup header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h2>B2B Diesel Truck Parts</h2>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Products</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
